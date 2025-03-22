a2fbdd432a0155c8690fe30b142fd221f6288f8d
junghwan <iseoulyou@gmail.com>
2025-03-22 18:44:51 +0900

    test2

---
+++ src/app/test/page.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+
+export default function test(){
+    return(
+        <div>
+            <h1 className="color:red">test page</h1>
+            <h2 className='color:blue'>Hello</h2>
+        </div>
+    )
+}
