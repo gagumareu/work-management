ca588f6f7629f17960d2095f973a50325eb626ae
junghwan <iseoulyou@gmail.com>
2025-03-27 16:47:33 +0900

    css update2

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -3,7 +3,7 @@ import './App.css';
 export default function test(){
     return(
         <div>
-            <h1 className="color:red">test page</h1>
+            <h1 className="text-3xl font-bold underline text-red-500">test page</h1>
             <h2 className='color:blue'>Hello</h2>
         </div>
     )
